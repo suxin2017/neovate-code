385be7f9f79ac938a85679b742dad640ef93ba22
阿平 <xierenhong@kuaishou.com>
2026-01-21 12:51:04 +0800

    feat: add  repair to safeParseJson (#700)

--- src/utils/safeParseJson.ts
+++ src/utils/safeParseJson.ts
@@ -1,7 +1,20 @@
+import createDebug from 'debug';
+import { jsonrepair } from 'jsonrepair';
+
+const debug = createDebug('neovate:utils:safeParseJson');
+
 export function safeParseJson(json: string) {
   try {
     return JSON.parse(json);
   } catch (_error) {
-    return {};
+    // try to repair the json
+    try {
+      debug('safeParseJson failed, trying to repair', _error);
+      const repairedJson = jsonrepair(json);
+      return JSON.parse(repairedJson);
+    } catch (_repairError) {
+      debug('safeParseJson failed, repair failed', _repairError);
+      return {};
+    }
   }
 }
